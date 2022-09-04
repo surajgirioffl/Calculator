4ee70acbef6d0addf9fd88e063b1e3023ae017c5
surajgirioffl <surajkumargiri2002@gmail.com>
2022-09-05 02:59:30 +0530

    :octocat:Initial: Initial Commit
    Initial Commit of the repository.
    
    Signed-off-by: @surajgirioffl

---
+++ src/main.py
@@ -0,0 +1,12 @@
+"""
+    Calculator (GUI) using Tkinter and Python 3.10.5
+     @author: Suraj Kumar Giri.
+     @Date: 05-09-2022 (Started)
+     @Time: 02: 34: 39 (Started)
+"""
+from tkinter import *
+root = Tk()  # master/root windows
+root.title("Calculator")
+root.geometry("600x600")
+
+root.mainloop()
