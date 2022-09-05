e0d0486743caf605b8320809e4d0ffe996ef451b
surajgirioffl <surajkumargiri2002@gmail.com>
2022-09-05 18:03:39 +0530

    :sparkles:Feat: add operator buttons in calculator GUI
    Add operator buttons in the calculator GUI program.
    
    Signed-off-by: @surajgirioffl

--- src/main.py
+++ src/main.py
@@ -28,6 +28,17 @@ def loadDigitIcons():
     return (zero, one, two, three, four, five, six, seven, eight, nine)
 
 
+def loadOperatorIcons():
+    abspath = "D:\Programming\Projects\Python\GUI Projects\Calculator\icons\operators\\"
+    add = PhotoImage(file=f"{abspath}add.png")
+    subtract = PhotoImage(file=f"{abspath}subtract.png")
+    multiply = PhotoImage(file=f"{abspath}multiply.png")
+    divide = PhotoImage(file=f"{abspath}divide.png")
+    percentage = PhotoImage(file=f"{abspath}percentage.png")
+    equalTo = PhotoImage(file=f"{abspath}equalTo.png")
+    return (add, subtract, multiply, divide, percentage, equalTo)
+
+
 digitIconsTuple = loadDigitIcons()
 zero_btn = Button(root, image=digitIconsTuple[0], border="0")
 one_btn = Button(root, image=digitIconsTuple[1], border="0")
@@ -40,23 +51,40 @@ seven_btn = Button(root, image=digitIconsTuple[7], border="0")
 eight_btn = Button(root, image=digitIconsTuple[8], border="0")
 nine_btn = Button(root, image=digitIconsTuple[9], border="0")
 
+addIcon, subtractIcon, multiplyIcon, divideIcon, percentageIcon, equalToIcon = loadOperatorIcons()
+add_btn = Button(root, image=addIcon, border="0")
+subtract_btn = Button(root, image=subtractIcon, border="0")
+multiply_btn = Button(root, image=multiplyIcon, border="0")
+divide_btn = Button(root, image=divideIcon, border="0")
+percentage_btn = Button(root, image=percentageIcon, border="0")
+equalTo_btn = Button(root, image=equalToIcon, border="0")
+
+
 # Packing the buttons
 # space between two button on x-axis (column width) = 120 units
 # space between two rows on y-axis (row height) = 120 units
+
+# 0th row
+percentage_btn.place(x=290, y=80)
+divide_btn.place(x=410, y=80)
 # first row
 seven_btn.place(x=50, y=200)
 eight_btn.place(x=170, y=200)
 nine_btn.place(x=290, y=200)
+multiply_btn.place(x=410, y=200)
 # 2nd row
 four_btn.place(x=50, y=320)
 five_btn.place(x=170, y=320)
 six_btn.place(x=290, y=320)
+subtract_btn.place(x=410, y=320)
 # 3rd row
 one_btn.place(x=50, y=440)
 two_btn.place(x=170, y=440)
 three_btn.place(x=290, y=440)
+add_btn.place(x=410, y=440)
 # 4th row
 zero_btn.place(x=170, y=560)
+equalTo_btn.place(x=410, y=560)
 
 
 root.mainloop()
